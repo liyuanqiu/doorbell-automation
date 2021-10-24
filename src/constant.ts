1bb432ff288654ae8bfe3aed87ec936f2efbdfcf
李元秋 <liyuanqiu@panka-jPC.fareast.corp.microsoft.com>
2021-10-24 14:21:35 +0800

    happy path

--- src/constant.ts
+++ src/constant.ts
@@ -1,2 +1,5 @@
 export const TCP_PORT = 8899;
-export const HTTP_PORT = 80;
+export const HTTP_PORT = 8080;
+export const REG_MSG = Buffer.from("64af0fb67164", "hex");
+export const OPEN_DOOR_STEP_1_MSG = Buffer.from("55AA000300020106", "hex");
+export const OPEN_DOOR_STEP_2_MSG = Buffer.from("55AA000300010106", "hex");
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,33 @@
 import { Socket, createServer } from "net";
+import { createServer as createServerHTTP } from "http";
 import { log, LogLevel } from "./log";
-import { TCP_PORT } from "./constant";
+import {
+  TCP_PORT,
+  HTTP_PORT,
+  REG_MSG,
+  OPEN_DOOR_STEP_1_MSG,
+  OPEN_DOOR_STEP_2_MSG,
+} from "./constant";
 
 let socket: Socket | null = null;
 
 function createTCPServer() {
   const server = createServer((_socket) => {
-    socket = _socket;
-    log(LogLevel.INFO, "Client connected!");
-    log(LogLevel.INFO, `Client address: ${JSON.stringify(socket.address())}`);
-    socket.on("end", () => {
-      log(LogLevel.INFO, "Client disconnected!");
+    log(LogLevel.INFO, "TCP_SERVER Client connected!");
+    log(
+      LogLevel.INFO,
+      `TCP_SERVER Client address: ${JSON.stringify(_socket.address())}`
+    );
+    _socket.on("end", () => {
+      log(LogLevel.INFO, "TCP_SERVER Client disconnected!");
+    });
+    _socket.on("data", (buf) => {
+      log(LogLevel.INFO, `TCP_SERVER Recv "${buf.toString("hex")}"`);
+      if (buf.equals(REG_MSG)) {
+        socket = _socket;
+        log(LogLevel.INFO, "TCP_SERVER Controller register successfully!");
+        return;
+      }
     });
   });
 
@@ -19,6 +36,36 @@ function createTCPServer() {
   });
 
   server.listen(TCP_PORT, () => {
-    log(LogLevel.INFO, `Listen on port [${TCP_PORT}]`);
+    log(LogLevel.INFO, `TCP_SERVER Listen on port [${TCP_PORT}]`);
   });
 }
+
+function createHTTPServer() {
+  const server = createServerHTTP((req, res) => {
+    log(LogLevel.INFO, `${req.method} ${req.url}`);
+    if (req.method === "GET" && req.url === "/open-door") {
+      log(LogLevel.INFO, "HTTP_SERVER Handling opening door...");
+      if (socket === null) {
+        log(LogLevel.INFO, "HTTP_SERVER No controller connected!");
+        res.end("No controller connected!");
+        return;
+      }
+      socket.write(OPEN_DOOR_STEP_1_MSG, () => {
+        setTimeout(() => {
+          socket.write(OPEN_DOOR_STEP_2_MSG, () => {
+            res.end("Command sent to controller.");
+          });
+        }, 200);
+      });
+      return;
+    }
+    res.end("Unrecognized command.");
+  });
+
+  server.listen(HTTP_PORT, () => {
+    log(LogLevel.INFO, `HTTP_SERVER Listen on port [${HTTP_PORT}]`);
+  });
+}
+
+createTCPServer();
+createHTTPServer();
